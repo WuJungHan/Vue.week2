1fe80224bf288f35b8410a1bc2c9e0385c0bbf6a
WuJungHan <eva29485577@gmail.com>
2021-05-25 14:06:26 +0800

    20210525pm14:06

--- js/login.js
+++ js/login.js
@@ -17,31 +17,17 @@ loginBtn.addEventListener('click', login);
 //登入函式
 function login(evant) {
   event.preventDefault();//如果事件可以被取消，就取消事件（即取消事件的預設行為）。 但不會影響事件的傳遞，事件仍會繼續傳遞。在這邊是取消submit提交表單
-  let username;
-  let password;
+  const username=loginEmail.value;
+  const password=loginPassword.value;
   checkPass();//呼叫檢查函式
-
+  
   const user = {//組成user物件
     username,
     password
   }
   //console.log(user);//驗證取得loginEmail.value , loginPassword.value
+  stopOrKeep(user);//呼叫函式 判斷是否中斷程式碼或繼續發送post
 
-  //發送 API 至遠端並登入（並儲存 Token）
-  //登入及驗證 - 登入 發送post請求 加上url 並在參數加上user(username password) 去做登入
-  //api:/admin/signin
-  axios.post(`${url}admin/signin`, user)
-    .then(res => {//回傳
-      console.log(res);//驗證登入是否成功 data.message 狀態顯示
-      loginStatus=res.data.message;//登入失敗或成功
-      console.log(loginStatus)//驗證
-      renderLoginStatus();//執行renderLoginStatus函式
-      //登入成功後 expired=到期日 token=憑證 (到期日過憑證就無使用) uid=實際儲存在後端使用的身分
-      const token = res.data.token;//取出token憑證
-      const expired=res.data.expired;//取出expired到期日
-      //onsole.log(token,expired);//驗證
-      document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;//儲存token跟expired到本地cookie,new Date(expired)可以將expired轉成日期格式
-    });
 }
 
 //取得 Token（Token 僅需要設定一次） test2替換成hexToken
@@ -63,7 +49,6 @@ function checkPass(){
   checkPassword.textContent=``;//將checkPassword清空
   }else{//不然
   checkPassword.textContent=`please enter your password`;//checkPassword寫上字串
-  return//中斷程式碼
   }
   
 }
@@ -82,3 +67,32 @@ function renderLoginStatus(){
   }
   loginPassword.value='';//清空密碼欄位
 }
+
+//axios 登入
+function axiosLogin(user){
+//發送 API 至遠端並登入（並儲存 Token）
+  //登入及驗證 - 登入 發送post請求 加上url 並在參數加上user(username password) 去做登入
+  //api:/admin/signin
+  axios.post(`${url}admin/signin`, user)
+    .then(res => {//回傳
+      console.log(res);//驗證登入是否成功 data.message 狀態顯示
+      loginStatus=res.data.message;//登入失敗或成功
+      console.log(loginStatus)//驗證
+      renderLoginStatus();//執行renderLoginStatus函式
+      //登入成功後 expired=到期日 token=憑證 (到期日過憑證就無使用) uid=實際儲存在後端使用的身分
+      const token = res.data.token;//取出token憑證
+      const expired=res.data.expired;//取出expired到期日
+      //onsole.log(token,expired);//驗證
+      document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;//儲存token跟expired到本地cookie,new Date(expired)可以將expired轉成日期格式
+    });
+}
+
+//中斷函式
+function stopOrKeep(user){
+  if(loginEmail.value==''||loginPassword.value==''){//如果欄位為空字串
+  return//中斷程式碼
+  }else{//不然
+  axiosLogin(user)//執行函式
+  }
+}
+
